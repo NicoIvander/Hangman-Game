56bef37d659a79e94abc3f21b8b185016faa5c7f
Nico Ivander Setiady <nico.setiady@student.jade-hs.de>
2024-10-30 17:28:10 +0100

    Step 3

--- Day 7 Hangman Project.py
+++ Day 7 Hangman Project.py
@@ -2,23 +2,35 @@ import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
-# TO-DO 1 - Randomly choose a word from word_list
 chosen_word = random.choice(word_list)
 print(chosen_word)
-
 
 placeholder = ""
 for letter in chosen_word :
     placeholder += "_"
 print(placeholder)
-guess = input("Guess a letter: ").lower()
 
+game_over = False
+correct_letter = []
 
-display = ""
-for letter in chosen_word:
-    if letter == guess:
-        display += guess
-    else:
-        display += "_"
+while not game_over:
+    guess = input("Guess a letter: ").lower()
 
-print(display)
+    display = ""
+    # TODO-2: Change the for loop so that you keep the previous correct letters in display.
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letter.append(letter)
+        elif letter in correct_letter:
+            display += letter
+        else:
+            display += "_"
+    print(correct_letter)
+    print(display)
+
+    if "_" not in display:
+        game_over = True
+        print("You Win!")
+
